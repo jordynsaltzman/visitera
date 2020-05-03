856056d0f506b693c11d5502ff52facf53c9e9fc
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-05-03 19:10:44 -0400

    installed dependencies, set up linter, express app, and not found/error middlewares

---
+++ server/src/middlewares.js
@@ -0,0 +1,21 @@
+const notFound = (req, res, next) => {
+  //creating error, setting the 404 status code, and passing it on to the error handler
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    //only show stack if we are in development bc it is a potential vulnerability to display code in production
+    stack: process.env.NODE_ENV === "production" ? ":-(" : error.stack,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
