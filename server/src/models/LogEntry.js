642dccf30c71d003950a457ea9ed23cb31c77f38
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-05-03 20:44:42 -0400

    created logEntry schema

---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const requiredNumber = {
+  type: Number,
+  required: true,
+};
+
+const logEntrySchema = new Schema(
+  {
+    title: requiredString,
+    description: requiredString,
+    comments: String,
+    image: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    latitude: requiredNumber,
+    longitude: requiredNumber,
+    visitDate: {
+      required: true,
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+
+module.exports = LogEntry;
