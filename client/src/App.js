18c280bbd7b8cf63851a795e9946e4c7cb9ec17b
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-05-04 11:36:34 -0400

    added markers and popups to mafor each log entry

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { listLogEntries } from "./API";
 import logo from "./images/Visitera.png";
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 37.7577,
-    longitude: -95,
-    zoom: 4,
+    latitude: 30,
+    longitude: -8,
+    zoom: 1.5,
   });
 
   useEffect(() => {
     (async () => {
-      const logEntries = await listLogEntries();
-      setLogEntries(logEntries);
-      console.log(logEntries);
+      const logEntriesList = await listLogEntries();
+      setLogEntries(logEntriesList);
+      console.log(logEntriesList);
     })();
   }, []);
 
@@ -33,6 +34,52 @@ const App = () => {
         alt="Visitera logo"
         style={{ height: "50px", padding: "15px" }}
       />
+      {logEntries.map((entry) => (
+        <>
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+          >
+            <div
+              onClick={() =>
+                setShowPopup({
+                  [entry._id]: true,
+                })
+              }
+            >
+              <img
+                className="marker"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                  //maybe set a max width and max height so when u zoom it doesnt get too big
+                }}
+                src="https://i.imgur.com/y0G5YTX.png"
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+            >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+                <small>
+                  Visited on {new Date(entry.visitDate).toLocaleDateString}
+                </small>
+              </div>
+            </Popup>
+          ) : null}
+        </>
+      ))}
     </ReactMapGL>
   );
 };
