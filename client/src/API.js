0c787ed3a0ece657d9722a64542a129dcfe6df56
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-05-11 15:38:33 -0400

    successfully debugged CORS error

--- client/src/API.js
+++ client/src/API.js
@@ -1,24 +1,37 @@
+import axios from "axios";
+
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:1337"
-    : "https://cors-anywhere.herokuapp.com/https://visitera-qmdkmf709.now.sh";
+    : "https://floating-dawn-92174.herokuapp.com";
 
-export async function listLogEntries() {
+export const listLogEntries = async () => {
   const response = await fetch(`${API_URL}/api/logs`);
-  return response.json();
-}
+  const data = await response.json();
+  return data;
+};
 
 export async function createLogEntry(entry) {
   const apiKey = entry.apiKey;
   delete entry.apiKey;
-  const response = await fetch(`${API_URL}/api/logs`, {
+  let data = JSON.stringify(entry);
+
+  const response = axios.post(`${API_URL}/api/logs`, data, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+      "X-API-KEY": apiKey,
+    },
+  });
+
+  /* const response = await axios.post(`${API_URL}/api/logs`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
       "X-API-KEY": apiKey,
     },
     body: JSON.stringify(entry),
-  });
+  }); */
   const json = await response.json();
   if (response.ok) {
     return json;
